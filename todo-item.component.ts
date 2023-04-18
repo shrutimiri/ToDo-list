c2591d8a98e2c0b8c75bff61d4ff50522a51a911
shrutimiri <131144737+shrutimiri@users.noreply.github.com>
2023-04-18 16:09:50 +0530

    Add files via upload

---
+++ todo-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/Todos';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+
+  @Input()  todo!: Todo;
+  @Output() todoDelete : EventEmitter<Todo> = new EventEmitter();
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(todo:Todo){
+    this.todoDelete.emit(todo);
+    console.log("onClick has been triggered")
+  }
+
+}
